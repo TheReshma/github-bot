2ba9b1f1014929e5b997cf7cd53d1ef635682ea7
Reshma <reshmashaik3011@gmail.com>
2022-05-14 22:01:04 +0530

    Handle errors : Done

--- index.js
+++ index.js
@@ -23,89 +23,80 @@ module.exports = (app) => {
   app.log.info("Yay, the app has loaded!");
   
   app.on('pull_request.opened', opened);
+  app.on('pull_request.closed', closed);
+
 
   const server_url = process.env.MORALIS_SERVER_ID ;
   const app_id = process.env.MORALIS_APPLICATION_ID;
   let comment ;
   
-    async function opened(context) {
+  async function opened(context) {
+    const prhead = String(context.payload.pull_request.title).trim();
+    const keywords = prhead.split(' ',2);
+    const essential = keywords[0];
+    const taskId = keywords[1];
+    if (essential == "Spect"){
+      const name = String(context.payload.pull_request.user.login);
+      const nameUrl = String(context.payload.pull_request.user.url);
+      const prLink = String(context.payload.pull_request.html_url);
 
-      const prhead = String(context.payload.pull_request.title).trim();
-      const keywords = prhead.split(' ',2);
-      const essential = keywords[0];
-      const taskId = keywords[1];
-
-      if (essential == "Spect"){
-
-        const name = String(context.payload.pull_request.user.login);
-        const nameUrl = String(context.payload.pull_request.user.url);
-        const prLink = String(context.payload.pull_request.html_url);
-
-        var data = {
-          "updates":{
-              "status": 200,
-          }
+      var data = {
+        "updates":{
+            "status": 200,
         }
-
-        var config = {
-          method: 'get',
-          url: `${server_url}/functions/updateCard?_ApplicationId=${app_id}&taskId=${taskId}&link=${prLink}&user=${name}`,
-          data: data
-        };
-
-        await axios(config)
-        .then(function (response) {
-          console.log(response.data);
-          if(response.status == 200){
-            comment = "Hey ["+name+"]("+nameUrl+"), your [Pull Request]("+prLink+") has been received by Spect!";
-          }else{
-            comment = "Hey ["+name+"]("+nameUrl+"), it looks like there's some issue with updating your progress";
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-
-        const issueComment = context.issue({
-          body: comment,
-        });
-        return context.octokit.issues.createComment(issueComment);
-        } 
       }
 
+      var config = {
+        method: 'get',
+        url: `${server_url}/functions/githubUpdateCard?_ApplicationId=${app_id}&taskId=${taskId}&link=${prLink}&user=${name}`,
+        data: data
+      };
 
-  app.on('pull_request.merged', merged);
+      await axios(config)
+      .then(function (response) {
+        console.log(response.data);
+        comment = "Hey "+name+", "+`${response.data.result}`;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
-    async function merged(context) {
-
-      const prhead = String(context.payload.pull_request.title).trim();
-      const keywords = prhead.split(' ',2);
-      const essential = keywords[0];
-      const taskId = keywords[1];
-
-      if (essential == "Spect"){
-
-        var data = {
-          "updates":{
-              "status": 205
-          }
-        }
-
-        var config = {
-          method: 'get',
-          url: `${server_url}/functions/updateCard?_ApplicationId=${app_id}&taskId=${taskId}&user=${name}`,
-          data: data
-        };
-
-        await axios(config)
-        .then(function (response) {
-          console.log(response.data);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+      const issueComment = context.issue({
+        body: comment,
+      });
+      return context.octokit.issues.createComment(issueComment);
       } 
     }
+
+  async function closed(context) {
+    const mergeState = context.payload.pull_request.merged;
+    const prhead = String(context.payload.pull_request.title).trim();
+    const prLink = String(context.payload.pull_request.html_url);
+    const name = String(context.payload.pull_request.user.login);
+    const keywords = prhead.split(' ',2);
+    const essential = keywords[0];
+    const taskId = keywords[1];
+
+    if (essential == "Spect" & mergeState == true){
+      var data = {
+        "updates":{
+            "status": 205
+        }
+      }
+      var config = {
+        method: 'get',
+        url: `${server_url}/functions/githubUpdateCard?_ApplicationId=${app_id}&taskId=${taskId}&link=${prLink}&user=${name}`,
+        data: data
+      };
+      await axios(config)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    } 
+  }
 };
 
   
